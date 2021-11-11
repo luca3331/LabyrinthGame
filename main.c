995e8bcbcdd790d8d7286328767d6d8bbc3506a1
Takamu <s17t235@stu.kagawa-u.ac.jp>
2021-11-11 20:41:28 +0900

    演習課題10

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #define MAP_SIZE 5
+#define ARRW_SIZE 5
+#define INPUT_MIN 0
+#define INPUT_MAX 4
 
 enum chip { PASS, STRT, GOAL, WALL, DARK, JUMP};
 const char* chip_disp[] = { " ", "S" , "G", "■", "*", "J"};
+const char* arrw_disp[] = { "","↓","↑","→","←" };
+
 enum chip map [MAP_SIZE][MAP_SIZE] = {
     {WALL,WALL,WALL,STRT,WALL},
     {WALL,JUMP,PASS,PASS,WALL},
@@ -10,6 +16,20 @@ enum chip map [MAP_SIZE][MAP_SIZE] = {
     {WALL,PASS,PASS,PASS,WALL},
     {WALL,GOAL,WALL,WALL,WALL},
 };
+int get_input(int min,int max){
+    int input;
+    printf("進行方向を選んでください．\n1:↓ 2:↑ 3:→ 4:←\n>");
+    while(1){
+        scanf("%d",&input);
+        if(input < INPUT_MAX && input > INPUT_MIN){
+            break;
+        }else{
+            printf("入力値が正しくありません．再入力してください.\n>");
+            input = -1;
+        }
+    }
+    return input;
+}
 void print_map(){
     int col,row;
     for(row = 0;row < MAP_SIZE;row++){
@@ -22,5 +42,7 @@ void print_map(){
 
 int main(void){
     print_map();
+    int input;
+    input = get_input(INPUT_MIN,INPUT_MAX);
     return 0;
 }
