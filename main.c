3fc2fc8eef90c622476bcae6f6801f94f4f3249b
Takamu <s17t235@stu.kagawa-u.ac.jp>
2021-11-11 21:55:52 +0900

    work13

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #define MAP_SIZE 5
+#define ARRW_SIZE 5
+#define INPUT_MIN 0
+#define INPUT_MAX 5
+
+typedef struct {
+    int x;
+    int y;
+} point;
 
 enum chip { PASS, STRT, GOAL, WALL, DARK, JUMP};
+enum arrw { EXIT,DOWN,UP,RIGHT,LEFT };
 const char* chip_disp[] = { " ", "S" , "G", "■", "*", "J"};
+const char* arrw_disp[] = { "","↓","↑","→","←" };
+
 enum chip map [MAP_SIZE][MAP_SIZE] = {
     {WALL,WALL,WALL,STRT,WALL},
     {WALL,JUMP,PASS,PASS,WALL},
@@ -10,17 +22,51 @@ enum chip map [MAP_SIZE][MAP_SIZE] = {
     {WALL,PASS,PASS,PASS,WALL},
     {WALL,GOAL,WALL,WALL,WALL},
 };
-void print_map(){
+int get_input(int min,int max){
+    int input;
+    printf("進行方向を選んでください．\n1:↓ 2:↑ 3:→ 4:←\n>");
+    while(1){
+        scanf("%d",&input);
+        if(input == 0){
+            break;
+        }
+        if(input < INPUT_MAX && input > INPUT_MIN){
+            break;
+        }else{
+            printf("入力値が正しくありません．再入力してください.\n>");
+            input = -1;
+        }
+    }
+    return input;
+}
+void print_map(point p,int dir){
     int col,row;
     for(row = 0;row < MAP_SIZE;row++){
         for(col = 0;col < MAP_SIZE;col++){
-            printf("%s",chip_disp[map[row][col]]);
+            if(col == p.x && row == p.y){
+                printf("%s",arrw_disp[dir]);}
+            else{printf("%s",chip_disp[map[row][col]]);}
         }
     puts("");
     }
 }
 
 int main(void){
-    print_map();
+    int game_end = 0;
+    point cur_p;
+    cur_p.x = 3;
+    cur_p.y = 0;
+    int dir = 0;
+    print_map(cur_p,dir);
+    while(!game_end){
+        int input;
+        input = get_input(INPUT_MIN,INPUT_MAX);
+        if(input == 1){cur_p.y += 1;dir = 1;}
+        if(input == 2){cur_p.y -= 1;dir = 2;}
+        if(input == 3){cur_p.x += 1;dir = 3;}
+        if(input == 4){cur_p.x -= 1;dir = 4;}
+        if(input == 0){game_end = 1;}
+        print_map(cur_p,dir);
+    }
     return 0;
 }
